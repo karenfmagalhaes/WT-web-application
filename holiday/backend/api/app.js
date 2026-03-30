ef6849282a7b1001d7b9ac48087164ebdb836540
Percy FM <athena.focaziomoran@student.griffith.ie>
2026-03-30 12:04:11 +0100

    "Working through bugs with imports and MongoDB connection"

--- holiday/backend/api/app.js
+++ holiday/backend/api/app.js
@@ -4,10 +4,10 @@
  * Authors: Karen Ferreira Magalhaes, Nataly Fonseca Mendes, Percy Focazio-Moran, Rafiq Abudulai
  */
 
-app.use(express.json()); //Allows Express to parse incoming JSON data automatically. 
+// app.use(express.json()); //Allows Express to parse incoming JSON data automatically.
 
-mongoose.connect(process.env.MONGO_URI) //Establishes a connection to the database (should use a .env). 
+// mongoose
+//   .connect(process.env.MONGO_URI) //Establishes a connection to the database (should use a .env).
 
-.then(() => console.log('MongoDB connected')) //Handle success and error scenarios for connection
-.catch((err) => console.log(err));
-
+//   .then(() => console.log("MongoDB connected")) //Handle success and error scenarios for connection
+//   .catch((err) => console.log(err));
